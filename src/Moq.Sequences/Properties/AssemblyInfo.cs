9cbc650902c27e9b4e507faa846910812ecaab03
Andrea <roundcrisis@gmail.com>
2010-08-06 11:25:56 +0100

    project layout modification strong naming had to change the moq version so it works with mine

---
+++ src/Moq.Sequences/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Moq.Sequences")]
+[assembly: AssemblyDescription("Supports checking that Moq setups are executed in sequence.")]
+[assembly: AssemblyProduct("Moq.Sequences")]
+[assembly: AssemblyCopyright("Declan Whelan © 2010")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4e1e90c1-9fdc-4aa1-be0a-46975884172e")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Moq.Sequences.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fb3e12dde7826786cea50acab656e6dec2aed971cd2f3c9bcc821041f6d802d21f6121f5ce1f9ef236944236be21a90b432ac5082ec68529b11f6a3bf76f8e43af31162c410153560b4b732a29a02dee5efe7111930fcd116c5c70bb65a027ace4389609b03254cd82a5d47d4e1f9fd3b2cc4bc3d34be884bdc8149f350252ae")]
